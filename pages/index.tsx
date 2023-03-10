70fd43b4f6d663a6182e6dd375f7b95e8a09ba74
Yam Jun Jie <yjjcoolcool@gmail.com>
2023-03-10 13:22:05 +0800

    Removed constant in index
    Moved it to within the Tabs element itself

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,41 +5,8 @@ import Tabs from '@/components/LayoutComponents/Tabs'
 import CardGridBackground from '@/components/LayoutComponents/CardGridBackground'
 import CardGrid from '@/components/LayoutComponents/CardGrid'
 import Heading from '@/components/LayoutComponents/Heading'
-import Image from 'next/image';
 import UnderConstruction from '@/components/LayoutComponents/UnderConstruction'
 import { quick_links } from '@/constants/links';
-
-const tabs = [
-  {
-    title: "Innovate",
-    content: <>
-      <p>Here in Limaru, we’re always trying out new things and exploring new ideas - whether it be new building techniques, new plugins, or new server events.</p>
-      <br/>
-      <p>In the image is our National Day Parade back in 2021, where we arranged a whole show with a parachute jump, a military parade, and floats designed by our community!</p>
-    </>,
-    image: <Image src="https://via.placeholder.com/150" alt="Tab 1" />,
-  },
-  {
-    title: "Create",
-    content: <>
-      <p>Limaru is a place for everyone to express their Creativity. Regardless of experience, our community is here to help you build your dream city together!</p>
-      <br/>
-      <p>In the image is Lake District in Mainland Limaru. Over the years, it has transformed into an area filled with modern architecture!</p>
-    </>,
-    image: <Image src="https://via.placeholder.com/150" alt="Tab 2" />,
-  },
-  {
-    title: "Experience",
-    content: <>
-      <p>What’s a city without the interaction? Limaru is filled with things to interact with - whether you would like to ride a train, visit an amusement park, or work at a glass factory, we’ve got you covered!</p>
-      <br/>
-      <p>In the image is the Glass Factory in Mainland Limaru. You can help out at the factory by converting sand into glass and earn some money!</p>
-    </>,
-    image: <Image src="https://via.placeholder.com/150" alt="Tab 3" />,
-  },
-];
-
-//A
 
 export default function Home() {
   return (
