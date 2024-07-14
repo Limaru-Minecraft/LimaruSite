8ad473c73bcfe8e546409266c8983a4bc12a3b19
Jun Jie Yam <yjjcoolcool@gmail.com>
2024-07-14 12:12:22 +0800

    Some error about next export? idk

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  ...(process.env.NODE_ENV === 'production' || process.env.EXPORT === 'true' ? { output: 'export' } : {}),
   reactStrictMode: true,
   images: {
     domains: ['via.placeholder.com'],
