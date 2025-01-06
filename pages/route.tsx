dd11bab5f39ea17fb431722483a4ee5d7dbf80f2
winsanmwtv <wntv2020@gmail.com>
2025-01-06 21:12:36 +0700

    do by comment (1)

--- pages/route.tsx
+++ pages/route.tsx
@@ -1,39 +1,26 @@
-import React from "react";
-import Head from "@/components/Head";
-import PageLayout from "@/components/PageLayout";
-import SectionBox from "@/components/SectionBox";
-import UnderConstruction from "@/components/LayoutComponents/UnderConstruction";
-import Heading from "@/components/LayoutComponents/Heading";
-import Pathfinder from "@/components/transportation/pathfinder";
-import Status from "@/components/transportation/status";
-import Route from "@/components/transportation/route";
+import Head from '@/components/Head'
+import PageLayout from '@/components/PageLayout'
+import SectionBox from '@/components/SectionBox'
+import CurrentRoute from '@/components/TransportationComponents/route'
+import TripPlanner from '@/components/TransportationComponents/pathfinder'
 
-const Home: React.FC = () => (
-    <>
-        <Head
-            title="Route Query | Limaru"
-            description="Pathfinder is help you to find which route you need to be taken."
-            author="winsanmwtv"
-            keywords="limaru, limaru city server, limaru minecraft server, transport limaru, limaru transport"
-        />
-        <PageLayout title="Metro Route Finder">
-            {/*<SectionBox>*/}
-            {/*    <UnderConstruction type="page" />*/}
-            {/*</SectionBox>*/}
-            <SectionBox >
-                <div style={{ position: 'relative', zIndex: 10 }}>
-                    <Pathfinder/>
-                </div>
-
-            </SectionBox>
-
-            <SectionBox >
-                <div style={{ position: 'relative', zIndex: 1 }}><Route/></div>
-
-            </SectionBox>
-        </PageLayout>
-    </>
-);
-
-// @ts-ignore
-export default Home;
+export default function Home() {
+    return (
+        <>
+            <Head
+                title="Route Query | Limaru"
+                description="Find your route by the destination!"
+                author="YJJcoolcool"
+                keywords="limaru, limaru city server, limaru minecraft server"
+            />
+            <PageLayout title='Transportation'>
+                <SectionBox>
+                    <TripPlanner/>
+                </SectionBox>
+                <SectionBox>
+                    <CurrentRoute/>
+                </SectionBox>
+            </PageLayout>
+        </>
+    )
+}
--- pages/transportation.tsx
+++ pages/transportation.tsx
@@ -1,37 +1,29 @@
-import React from "react";
-import Head from "@/components/Head";
-import PageLayout from "@/components/PageLayout";
-import SectionBox from "@/components/SectionBox";
-import UnderConstruction from "@/components/LayoutComponents/UnderConstruction";
-import Heading from "@/components/LayoutComponents/Heading";
-import Pathfinder from "@/components/transportation/pathfinder";
-import Status from "@/components/transportation/status";
+import Head from '@/components/Head'
+import PageLayout from '@/components/PageLayout'
+import SectionBox from '@/components/SectionBox'
+import Heading from '@/components/LayoutComponents/Heading'
+import Status from '@/components/TransportationComponents/status'
+import TripPlanner from '@/components/TransportationComponents/pathfinder'
 
-const Home: React.FC = () => (
-    <>
-        <Head
-            title="Transportation | Limaru"
-            description="Transportation is a way to move people around Limaru."
-            author="YJJcoolcool, winsanmwtv"
-            keywords="limaru, limaru city server, limaru minecraft server, transport limaru, limaru transport"
-        />
-        <PageLayout title="Transportation">
-            {/*<SectionBox>*/}
-            {/*    <UnderConstruction type="page" />*/}
-            {/*</SectionBox>*/}
-            <SectionBox>
-                <Heading level={2}>Find your route</Heading>
-                <div style={{ position: 'relative', zIndex: 10 }}><Pathfinder/></div>
-
-
-            </SectionBox>
-
-            <SectionBox>
-                <Heading level={2}>Transit Status</Heading>
-                <div style={{position: 'relative', zIndex: 1}}><Status/></div>
-            </SectionBox>
-        </PageLayout>
-    </>
-);
-
-export default Home;
+export default function Home() {
+    return (
+        <>
+            <Head
+                title="Transportation | Limaru"
+                description="Ride Limaru's extensive transport network!"
+                author="YJJcoolcool"
+                keywords="limaru, limaru city server, limaru minecraft server"
+            />
+            <PageLayout title='Transportation'>
+                <SectionBox>
+                    <Heading level={2}>Find your route</Heading>
+                    <TripPlanner/>
+                </SectionBox>
+                <SectionBox>
+                    <Heading level={2}>Transit Status</Heading>
+                    <Status/>
+                </SectionBox>
+            </PageLayout>
+        </>
+    )
+}
