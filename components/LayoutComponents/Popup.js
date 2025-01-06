76ec0c281654266c0a8f05fa1f06e095f3cbf8af
Phongsiri Loedphongthai <77370170+winsanmwtv@users.noreply.github.com>
2025-01-06 07:33:56 +0700

    Update Popup.js

--- components/LayoutComponents/Popup.js
+++ components/LayoutComponents/Popup.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function Popup({ content }){
+export default function Popup({ content }) {
   return (
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center">
       <div className="p-8 border w-96 shadow-lg rounded-md bg-white">
