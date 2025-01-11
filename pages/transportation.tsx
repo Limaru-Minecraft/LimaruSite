7642d2ec88de4ac293594b4e34db78e1964fbb2d
Jun Jie Yam <yjjcoolcool@gmail.com>
2025-01-11 15:35:14 +0800

    Organise files & code formatting, changed some words

--- pages/transportation.tsx
+++ pages/transportation.tsx
@@ -1,29 +1,29 @@
-import Head from '@/components/Head'
-import PageLayout from '@/components/PageLayout'
-import SectionBox from '@/components/SectionBox'
-import Heading from '@/components/LayoutComponents/Heading'
-import Status from '@/components/TransportationComponents/status'
-import TripPlanner from '@/components/TransportationComponents/pathfinder'
+import Head from "@/components/Head";
+import PageLayout from "@/components/PageLayout";
+import SectionBox from "@/components/SectionBox";
+import Heading from "@/components/LayoutComponents/Heading";
+import Status from "@/components/TransportationComponents/status";
+import TripPlanner from "@/components/TransportationComponents/pathfinder";
 
 export default function Home() {
-    return (
-        <>
-            <Head
-                title="Transportation | Limaru"
-                description="Ride Limaru's extensive transport network!"
-                author="YJJcoolcool"
-                keywords="limaru, limaru city server, limaru minecraft server"
-            />
-            <PageLayout title='Transportation'>
-                <SectionBox>
-                    <Heading level={2}>Find your route</Heading>
-                    <TripPlanner/>
-                </SectionBox>
-                <SectionBox>
-                    <Heading level={2}>Transit Status</Heading>
-                    <Status/>
-                </SectionBox>
-            </PageLayout>
-        </>
-    )
+  return (
+    <>
+      <Head
+        title="Transportation | Limaru"
+        description="Ride Limaru's extensive transport network!"
+        author="YJJcoolcool"
+        keywords="limaru, limaru city server, limaru minecraft server"
+      />
+      <PageLayout title="Transportation">
+        <SectionBox>
+          <Heading level={2}>Route Planner</Heading>
+          <TripPlanner />
+        </SectionBox>
+        <SectionBox>
+          <Heading level={2}>Service Status</Heading>
+          <Status />
+        </SectionBox>
+      </PageLayout>
+    </>
+  );
 }
